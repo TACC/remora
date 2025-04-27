829118cea8828962e09063af6e15960d4e61e1ce
Kent Milfeld <milfeld@tacc.utexas.edu>
2025-04-27 15:20:19 -0500

    Fixed numa, now avoids warning from numastat -c/-m. Now only looks at non-zero NODES. GPU module now reports for each gpu_id.

--- src/C_data_collectors_src/data_collect_numa.c
+++ src/C_data_collectors_src/data_collect_numa.c
@@ -33,15 +33,61 @@ int main(int argc, char *argv[])
   clock_gettime(CLOCK_REALTIME, &now);
   double time_new = ((double)now.tv_sec*1e9 + now.tv_nsec) / 1000000000;
 
+  // get number of non-zero NODES with "numastat -z" by
+  // counting the number of words "Node 0 Node 1 ... Total" inline 3.
+  int nNodes, nNodesP1;
+  if (!(file = popen("numastat -z  | sed -n 3p  | wc -w", "r"))) { exit(1); }
+  fgets ( line, sizeof line, file );
+  nNodes=atoi(line); nNodes=(nNodes-1)/2; nNodesP1= nNodes + 1;
+  pclose(file);
 
-//  Extract numasat -m data
+  // Now form numastat command which removed all lines before "----- ------ ..." line
+  // which separates data from initial warning.
+  //                 numastat -c | awk -v n=3 '/-------/{flag=1} flag {for(i=1;i<=n;i++) printf "%s ", $i; printf "%s", $NF; print ""}'
 
-  if (!(file = popen("numastat -c", "r"))) { exit(1); }
+  char numastat_m[256];
+  char numastat_c[256];
+
+  sprintf(numastat_m, "%s%d %s", \
+          "numastat -m | awk -v n=", nNodesP1, \
+          "'/-------/{flag=1} flag {for(i=1;i<=n;i++) printf \"%s \", $i; printf \"%s\", $NF; print \"\"}'");
+  sprintf(numastat_c, "%s%d %s", \
+          "numastat -c | awk -v n=", nNodesP1, \
+          "'/-------/{flag=1} flag {for(i=1;i<=n;i++) printf \"%s \", $i; printf \"%s\", $NF; print \"\"}'");
+
+//  Extract numasat -c data
+
+  if (!(file = popen( numastat_m,   "r"))) { exit(1); }
 
   lineno=0; row=0;
   while ( fgets ( line, sizeof line, file ) != NULL ){
-    if(lineno>3 && lineno<7){                            //printf("read: %s\n",line);
-      sscanf(line, "%[^\n]s",str);
+    if(lineno>1 && lineno<34){                          //printf("read: %d %s",lineno,line);
+
+      char * token = strtok(line, " "); // tokenize
+      token = strtok(NULL, " ");        // skip 1st token, identifier keyword
+      i=0;
+
+      while( token != NULL ) { 
+        sscanf(token,"%f",  &numaStatM[row][i]);
+        token = strtok(NULL, " ");
+        i++;
+      }   
+      row++; 
+    } // end selecting line 1-32
+    lineno++;
+
+  } // end while getting line  
+
+  pclose(file);
+
+
+//  Extract numasat -c data
+
+  if (!(file = popen( numastat_c,   "r"))) { exit(1); }
+
+  lineno=0; row=0;
+  while ( fgets ( line, sizeof line, file ) != NULL ){
+    if(lineno>0 && lineno<4){                            //printf("read: %s\n",line);
 
       char * token = strtok(line, " "); // tokenize
       token = strtok(NULL, " ");        // skip 1st token, identifier keyword
@@ -55,38 +101,10 @@ int main(int argc, char *argv[])
       if(row == 0) node_count=i-1;  // #nodes=#values less 1, last value  is a total).
       row++; 
 
-    } // end selecting line 4-6
+    } // end selecting line 1-3
     lineno++;
 
   } // end while getting line   
-
-  pclose(file);
-
-
-//  Extract numasat -m data
-
-  if (!(file = popen("numastat -m", "r"))) { exit(1); }
-
-  lineno=0; row=0;
-  while ( fgets ( line, sizeof line, file ) != NULL ){
-    if(lineno>3 && lineno<30){                          //printf("read: %d %s",lineno,line);
-      sscanf(line, "%[^\n]s",str);
-
-      char * token = strtok(line, " "); // tokenize
-      token = strtok(NULL, " ");        // skip 1st token, identifier keyword
-      i=0;
-
-      while( token != NULL ) { 
-        sscanf(token,"%f",  &numaStatM[row][i]);
-        token = strtok(NULL, " ");
-        i++;
-      }   
-      row++; 
-
-    } // end selecting line 4-29
-    lineno++;
-
-  } // end while getting line  
 
   pclose(file);
 
@@ -103,9 +121,9 @@ int main(int argc, char *argv[])
 %15ld %15ld %15ld %15ld %15ld %15ld %15ld %15ld %15ld \
 %10.2f %10.2f %10.2f %10.2f %10.2f %10.2f \n", \
     time_new,node_count, \
-    M(13,0),M(13,1),M(13,2), M(15,0),M(15,1),M(15,2), M(25,0),M(25,1),M(25,2), \
+    M(12,0),M(12,1),M(12,2), M(14,0),M(14,1),M(14,2), M(24,0),M(24,1),M(24,2), \
     C(0,0),C(0,1),C(0,2), C(1,0),C(1,1),C(1,2), C(2,0),C(2,1),C(2,2), \
-    M(1,0),M(1,1),M(1,2), M(2,0),M(2,1),M(2,2) );
+    M(0,0),M(0,1),M(0,2), M(1,0),M(1,1),M(1,2) );
 
   return EXIT_SUCCESS;
 }
